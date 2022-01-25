e2772cff920960e6ce9ca6b61c25520468f28fac
Mariane Felix <marianefelix59@gmail.com>
2022-01-25 18:43:54 -0300

    Altera DioClient #33

--- app/lib/services/api.dart
+++ app/lib/services/api.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 class DioClient {
-  final Dio api = Dio(
+  static final Dio dio = Dio(
     BaseOptions(
       baseUrl: 'https://saveme/api', // TO DO: alterar quando api for hospedada 
       headers: {'Accept': 'application/json'},
