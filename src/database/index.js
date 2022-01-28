65083258d4f1d8846e22a9d229af961ec9eb3f6d
Mariane Felix <marianefelix59@gmail.com>
2022-01-28 15:51:01 -0300

    Altera index de database

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,6 @@
-import knex from "knex";
-import "dotenv/config";
+const knex = require("knex");
+const knexfile = require("../../knexfile");
 
-const db = knex({
-  client: "pg",
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
-  useNullAsDefault: true,
-});
+const connection = knex(knexfile);
 
-export default db;
+module.exports = connection;
